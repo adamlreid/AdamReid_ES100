eb19261a1fb3d4039d6ace23dab1aa1a72a9f685
Adam Reid <90157381+adamlreid@users.noreply.github.com>
2024-02-25 14:57:06 -0500

    Delete deviceFINAL.ino

--- deviceFINAL.ino
+++
@@ -1,6 +0,0 @@
-// ES100
-// Adam Reid
-// Arduino code to detect traffic and report air quality data
-
-// Designed for use with Elegoo MEGA 2560, 
-// 2x PMS5003 sensors, HC-SR04 sensor, DHT11 sensor
